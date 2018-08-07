30b71b0bfcf3b8ce03876305d16ee05ac5e8adba
JackyWu47 <jackyzero2000@hotmail.com>
2018-08-07 11:17:20 +0800

    Initial commit

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            for (i = 6; i <= 15; i++)
+            {
+                k = 1;
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        k = 0;
+                        break;
+                    }
+                }
+                if (k == 1)
+                {
+                    Console.Write(i);
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
